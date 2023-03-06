e71d739675a93a7295edd164b252a50117ab10e0
Alexandr <kromval6@gmail.com>
2023-03-06 14:59:43 +0600

    06.03

---
+++ Task4/src/test/java/org/example/IFuncIArgumentTest.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IFuncIArgumentTest {
+
+    @Test
+    public void testLinearFunction() {
+        LinearIFunc linearFunction = new LinearIFunc(1, 2, 1, 10);
+       // assertEquals(linearFunction.getValue(2), 4);
+        assertTrue(Math.abs(linearFunction.getValue(2) - 4) < 1E-5);
+    }
+
+    @Test
+    public void testExpFunction() {
+        ExpFunc expFunction = new ExpFunc(1, 2, 1, 0);
+        assertTrue( Math.abs(expFunction.getValue(2) - 7.39) < 0.001);
+    }
+
+    @Test
+    public void testSinFunction() {
+        SinFunc sinFunction = new SinFunc(0, 1, 1, 1);
+        assertTrue(sinFunction.getValue(0.1) - 0.099 < 0.001);
+    }
+
+    @Test
+    public void testRationalFunction() {
+        RationalFunc rationalFunction = new RationalFunc(10, 2, 1,1, 0, 10);
+        assertTrue(rationalFunction.getValue(2) - 7.333 < 0.001);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testError() {
+        ExpFunc expFunction = new ExpFunc(1, 2, 1, 0);
+        expFunction.getValue(100);
+    }
+}
